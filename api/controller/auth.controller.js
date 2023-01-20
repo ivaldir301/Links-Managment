59db65cd064273ed511723532f3d34537f998e93
Ivaldir Batalha <ivaldir@Ivaldirs-MBP.home>
2023-01-20 21:26:05 -0100

    Fixing some errors

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -37,7 +37,7 @@ const authController = {
 
             const { password1: hash, id, name } = user[0]
 
-            const checkedPassword = await bcrypt.compare(password1, hash)
+            const checkedPassword = await bcrypt.compare(password, hash)
 
             if(checkedPassword){
                 const acessToken = jwt.sign({ userId: id }, 'j23423lkjfwljer43rnsfgkl45', { expiresIn: '1h' });
